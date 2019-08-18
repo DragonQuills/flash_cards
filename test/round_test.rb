df0fbb8692b80f2ad60bd4efbcd4d2477c474600
Kayden Drake <root@localhost.localdomain>
2019-08-18 16:03:32 -0600

    Adds test for round and initialization and empty round file

---
+++ test/round_test.rb
@@ -0,0 +1,25 @@
+require 'minitest'
+require 'minitest/autorun'
+
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+require './lib/turn'
+
+class RoundTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("Who is the cutest cat in the whole world?", "Crona", :Pop_Culture)
+    @card_2 = Card.new("What is the corp that cares?", "Evo", :Corporate_Knowledge)
+    @card_3 = Card.new("Which corp will kill you if you fail a run?", "Mitsuhama", :Corporate_Knowledge)
+
+    @cards = [@card_1, @card_2, @card_3]
+
+    @deck = Deck.new(@cards)
+
+    @round = Round.new(@deck)
+  end
+
+  def test_deck_exists
+    assert_instance_of Deck, @deck
+  end
+end
